4db75ebc758a3ea8cc1880d88c809fe3b48458ea
Anjali470 <anjalikonkala001@gmail.com>
2026-06-01 21:20:51 +0530

    Added migrations and login functionality

--- todo/views.py
+++ todo/views.py
@@ -3,4 +3,14 @@ from django.shortcuts import render, redirect
 from .models import ToDo
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('todo.html')
+        else:
+            return render(request, 'login.html', {'error': 'Invalid username or password'})
+    else:
+        return render(request, 'login.html')
